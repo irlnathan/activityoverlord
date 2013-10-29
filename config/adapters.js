defaa3fb9b9f143a0e0a2ceadf448a84f1238c4f
irlnathan <irlnathan@gmail.com>
2013-10-29 11:43:37 -0500

    heroku redis deploy

--- config/adapters.js
+++ config/adapters.js
@@ -16,12 +16,13 @@ module.exports.adapters = {
 
   // // If you leave the adapter config unspecified 
   // // in a model definition, 'default' will be used.
-  // 'default': 'disk',
+   'default': 'mongo',
 
-  // // In-memory adapter for DEVELOPMENT ONLY
-  // memory: {
-  //   module: 'sails-memory'
-  // },
+   mongo: {
+      module   : 'sails-mongo',
+      url: process.env.DB_URL,
+      schema   : true
+    }
 
   // // Persistent adapter for DEVELOPMENT ONLY
   // // (data IS preserved when the server shuts down)
--- config/session.js
+++ config/session.js
@@ -15,24 +15,27 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: 'd494d185735d00432bc3485d32bd5ca8'
+  secret: 'd494d185735d00432bc3485d32bd5ca8',
 
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
+  adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
+
+
   //
-  // host: 'localhost',
-  // port: 6379,
+  host: process.env.REDIS_HOST, //'soldierfish.redistogo.com', // to the right of @
+  port: process.env.REDIS_PORT,//9599, // port found in heroku redis to go
   // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
+  db: process.env.REDIS_DB, //'redistogo',
+  pass: process.env.REDIS_PASS //'d5d68502e87bf36e5d6d25d9c0f37b5a' //password is to the left of @
   // prefix: 'sess:'
 
+// you should REDIS_HOST
 
   // Uncomment the following lines to use your Mongo adapter as a session store
   // adapter: 'mongo',
